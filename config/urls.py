a3941c5ed287f55105495e647a9d8e3a60490c10
Vinyzada <vinyescolas@gmail.com>
2024-03-21 16:45:38 -0300

    Crud Editora & Aula 8.0

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from livraria.views import CategoriaViewSet, EditoraViewSet
+
+router = DefaultRouter()
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include(router.urls))
 ]
--- livraria/views.py
+++ livraria/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from livraria.models import Categoria, Editora
+from livraria.serializers import CategoriaSerializer, EditoraSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+
