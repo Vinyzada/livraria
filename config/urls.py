43f301affa66156a008d7f6d489d3e235d0285ec
Vinyzada <vinyescolas@gmail.com>
2024-04-02 13:23:18 -0300

    Feature: Aula 18 - swagger & redoc

--- config/urls.py
+++ config/urls.py
@@ -7,10 +7,17 @@ from django.conf.urls.static import static
 from uploader.router import router as uploader_router
 
 from rest_framework.routers import DefaultRouter
-from usuario.router import router as usuario_router
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+)
+
+from usuario.router import router as usuario_router
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
 )
 
 from livraria.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
@@ -28,6 +35,21 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include(usuario_router.urls)),
+
+    # OpenAPI 3
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("api/", include(router.urls)),
 ]
 
-urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_ENDPOINT,
+                      document_root=settings.MEDIA_ROOT)
